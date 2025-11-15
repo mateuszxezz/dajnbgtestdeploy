4feedf23115bb9f896d26aef56452f7ba866d466
mateuszxezz <mateusbezerra092008@gmail.com>
2025-11-15 16:16:21 -0300

    mdssssss

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-55#inejyy^-p6bdv5w@m)l%%4(r0l7-v^f8ii08tih2dv04&cj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
 
